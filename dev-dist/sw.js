59847be0423775bdb340ea0da720b5105cf3477e
Ftah_Elm <gragon.fire99@gmail.com>
2026-08-25 16:42:33 -0700

    Update website

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0oa68prme58"
+    "revision": "0.4sfp8umlf1"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
